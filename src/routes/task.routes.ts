2b5134d5b242532fd180c510c9cf5450f9143646
Fernando Leon <ferzleon51@gmail.com>
2022-01-19 00:07:05 -0600

    implementando las rutas

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { getTasks} from "../controllers/task.controller";
+import { getTasks, createTask, getTask, deleteTask, count, updateTask} from "../controllers/task.controller";
 
 const router = Router()
 
 router.get('/tasks', getTasks)
-router.get('/tasks/count', (req, res) => res.send('Hello World!!'))
-router.post('/tasks', (req, res) => res.send('Hello World!!'))
-router.get('/tasks/:id', (req, res) => res.send('Hello World!!'))
-router.delete('/tasks/:id', (req, res) => res.send('Hello World!!'))
-router.put('/tasks/:id', (req, res) => res.send('Hello World!!'))
+router.get('/tasks/count', count)
+router.post('/tasks', createTask)
+router.get('/tasks/:id', getTask)
+router.delete('/tasks/:id', deleteTask)
+router.put('/tasks/:id', updateTask)
 
 export default router
