d7baa87d75792eb61705e44dfb1d67f6363bd34e
Fernando Leon <ferzleon51@gmail.com>
2022-01-19 00:23:18 -0600

    implementando Swagger basico

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,11 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 
+// Swagger
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+import { options } from "./swaggerOptions";
+
 
 import tasksRoutes from "./routes/task.routes";
 
@@ -13,8 +18,10 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
+const specs = swaggerJsDoc(options);
 
 // Routes
 app.use(tasksRoutes);
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 export default app
